e533eef7f8f474d7ebbd2ff3e4152b74212c5ca2
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-23 18:10:45 -0500

    :sparkles: cli params
    gonna watch movie with family

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,48 @@
+use clap::Parser;
 use std::{fs, io::prelude::*, net};
 
 static TEMPLATE: &'static str = include_str!("index.html");
 
+#[derive(Parser, Debug)]
+#[clap(
+    author = "UltiRequiem",
+    version = "0.1.0",
+    about = "Easily run React snippets"
+)]
+struct Args {
+    #[clap(short, long)]
+    file: String,
+    #[clap(short, long, default_value_t = 8080)]
+    port: u16,
+}
+
+fn react_app(file: String) -> String {
+    let app = fs::read_to_string(file).unwrap();
+    TEMPLATE.replace("// APP", &app)
+}
+
 fn main() {
-    let listener = net::TcpListener::bind("127.0.0.1:3000").expect("Could not bind to port");
+    let args = Args::parse();
+    let app = react_app(args.file);
+    let port = format!("127.0.0.1:{}", args.port);
+    let listener = net::TcpListener::bind(&port).expect("Could not bind to port");
+
+    println!("Listening on {}", &port);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        handle_connection(stream, &app);
+        print!("Ping!");
     }
 }
 
-fn handle_connection(mut stream: net::TcpStream) {
+fn handle_connection(mut stream: net::TcpStream, app: &String) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-
-    let app = fs::read_to_string("examples/simple.js").unwrap();
-
-    let contents = TEMPLATE.replace("// APP", &app);
-
     let response = format!(
         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
+        app.len(),
+        app
     );
 
     stream.write(response.as_bytes()).unwrap();
