ee7753c6684ff7705908d72bf79ccaf9c2056a59
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-26 14:47:10 -0500

    :construction: not working

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,16 @@
 use colored::Colorize;
 use open::that;
-use std::{io::prelude::*, net};
+use std::{
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+};
+use tungstenite::{
+    accept_hdr,
+    handshake::server::{Request, Response},
+};
 
 pub fn serve(app: &str, addr: &str, nobrowser: bool) {
-    let listener = net::TcpListener::bind(&addr)
+    let server = TcpListener::bind(&addr)
         .unwrap_or_else(|e| panic!("Port {} is already used: {}", &addr, e));
 
     let url = format!("http://{}", &addr);
@@ -19,28 +26,47 @@ pub fn serve(app: &str, addr: &str, nobrowser: bool) {
         });
     };
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream, &app);
+    for stream in server.incoming() {
+        handle_connection(stream.unwrap(), &app);
     }
 }
 
-fn handle_connection(mut stream: net::TcpStream, app: &str) {
-    match stream.read(&mut [0; 1024]) {
-        Ok(_) => println!("{}", "Request received.".green()),
-        Err(error) => panic!("Error reading the stream: {}", error),
-    };
+fn handle_connection(mut stream: TcpStream, app: &str) {
+    let mut buffer = [0; 1024];
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        app.len(),
-        app
-    );
+    stream
+        .read(&mut buffer)
+        .unwrap_or_else(|e| panic!("Error reading the stream: {}", e));
 
-    match stream.write(response.as_bytes()) {
-        Ok(_) => println!("[{}]", "Ping!".green()),
-        Err(error) => panic!("Could not write to stream: {}.", error),
-    };
+    let root_route = b"GET / HTTP/1.1\r\n";
+    let ws_route = b"GET /ws HTTP/1.1\r\n";
+
+    if buffer.starts_with(root_route) {
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            app.len(),
+            app
+        );
+
+        match stream.write(response.as_bytes()) {
+            Ok(_) => println!("[{}]", "Ping!".green()),
+            Err(error) => panic!("Could not write to stream: {}.", error),
+        };
+    } else if buffer.starts_with(ws_route) {
+        let callback = |_req: &Request, mut response: Response| {
+            let headers = response.headers_mut();
+            headers.append("data", "reload".parse().unwrap());
+            Ok(response)
+        };
+        let mut ws = accept_hdr(&stream, callback)
+            .unwrap_or_else(|e| panic!("Error accepting handshake: {:?}", e));
+        loop {
+            let msg = ws.read_message().unwrap();
+            if msg.is_binary() || msg.is_text() {
+                ws.write_message(msg).unwrap();
+            }
+        }
+    }
 
     stream.flush().unwrap();
 }
