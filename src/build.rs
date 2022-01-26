b5eb7a3fa2984024a166d2865eb5293e0e888790
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-26 14:51:05 -0500

    :recycle: Update build process

--- src/build.rs
+++ src/build.rs
@@ -10,15 +10,13 @@ fn is_valid_url(url: &str) -> bool {
 
 pub async fn react_app(file: &str) -> String {
     let app = if is_valid_url(file) {
-        let resp = get(&*file)
+        let resp = get(file)
             .send()
             .unwrap_or_else(|e| panic!("Error fetching \"{}\": {:?}", file, e));
 
-        let app = resp
-            .as_str()
-            .unwrap_or_else(|e| panic!("Error parsing response as string: {:?}", e));
-
-        app.to_owned()
+        resp.as_str()
+            .unwrap_or_else(|e| panic!("Error parsing response as string: {:?}", e))
+            .to_owned()
     } else {
         fs::read_to_string(file)
             .unwrap_or_else(|e| panic!("Could not read file \"{}\": {:?}.", file, e))
