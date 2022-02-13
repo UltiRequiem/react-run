321c077d2535d5a265a752da9d64169a6573d562
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-12 20:48:30 -0500

    :sparkles: Install from npm

---
+++ binary.js
@@ -0,0 +1,75 @@
+const { Binary } = require("@cloudflare/binary-install");
+const os = require("os");
+const cTable = require("console.table");
+
+const error = (msg) => {
+  console.error(msg);
+  process.exit(1);
+};
+
+const { version, name, repository } = require("./package.json");
+
+const supportedPlatforms = [
+  {
+    TYPE: "Windows_NT",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-pc-windows-msvc",
+  },
+  {
+    TYPE: "Linux",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-unknown-linux-musl",
+  },
+  {
+    TYPE: "Darwin",
+    ARCHITECTURE: "x64",
+    RUST_TARGET: "x86_64-apple-darwin",
+  },
+];
+
+const getPlatform = () => {
+  const type = os.type();
+  const architecture = os.arch();
+
+  for (let index in supportedPlatforms) {
+    let supportedPlatform = supportedPlatforms[index];
+    if (
+      type === supportedPlatform.TYPE &&
+      architecture === supportedPlatform.ARCHITECTURE
+    ) {
+      return supportedPlatform.RUST_TARGET;
+    }
+  }
+
+  error(
+    `Platform with type "${type}" and architecture "${architecture}" is not supported by ${name}.\nYour system must be one of the following:\n\n${cTable.getTable(
+      supportedPlatforms
+    )}`
+  );
+};
+
+const getBinary = () => {
+  const platform = getPlatform();
+  const url = `${repository.url}/releases/download/v${version}/${name}-v${version}-${platform}.tar.gz`;
+  return new Binary(url, { name });
+};
+
+const binary = getBinary();
+
+const run = () => {
+  binary.run();
+};
+
+const install = () => {
+  binary.install();
+};
+
+const uninstall = () => {
+  binary.uninstall();
+};
+
+module.exports = {
+  install,
+  run,
+  uninstall,
+};
